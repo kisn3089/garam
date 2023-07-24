5574e37a5517ffca16d50f524d161d01cc29e41e
Stemps <kisn3089@naver.com>
2023-07-24 22:07:11 +0900

    resolve import / use let / absolute paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,45 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  settings: {
+    'react': {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  plugins: ['react-refresh', 'react', 'react-hooks'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'import/no-unresolved': 'off',
   },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
+import Button from '@atoms/Button/Button';
 
 function App() {
+  let test = 12;
+
   return (
     <>
-      <video />
+      <Button />
     </>
   );
 }
